f813aa1e8d8568659a79e19dbc62d1d3df1e482c
Artsub <90512456+Artsub@users.noreply.github.com>
2025-05-12 14:56:18 +0300

    Delete CourseBD directory

--- CourseBD/backend/app/schemas/booking.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class BookingCreate(BaseModel):
-    user_id: int
-    event_id: int
-    booking_date: datetime
-    payment_status: bool = False
