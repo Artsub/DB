c1775d9e066507e575bbaf3be709d174ecd04415
Artsub <90512456+Artsub@users.noreply.github.com>
2025-05-12 14:56:26 +0300

    Delete CourseProjectFinal directory

--- CourseProjectFinal/backend/app/schemas/token.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
